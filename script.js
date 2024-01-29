9579a71d2a76de241220733cfd7f71076b438278
Vidit Purohit <vidit01purohit@gmail.com>
2024-01-29 17:46:38 +0530

    Initial work for Extension is done.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//Fetch the source code using javascript
+//const src_code=
+
+const url="https://localhost:5000/predict/ {src_code}"
+fetch(url)
+    .then(data => data.json())
+    .then(patternData => {
+        const mainText = jokeData.attachments[0].text;
+        const patternElement = document.getElementById('pattern');
+
+        patternElement.innerHTML = mainText;
+    })
