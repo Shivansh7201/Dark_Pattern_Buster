f6525fb1ab5b81e4afe5afcab4783b92212989d7
Vidit Purohit <107619125+vidit01purohit@users.noreply.github.com>
2024-02-05 01:51:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-//current_url=window.location.href;
-const current_url = "https://www.flipkart.com/"
-const url="https://localhost:4040/predict?url="+current_url;
-async function fetchData() {
-    const res=await fetch(url);
-    const record=await res.json();
-    document.getElementById("pattern").innerHTML=record;
-}
-fetchData();
